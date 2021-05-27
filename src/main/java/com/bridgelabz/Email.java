5b789924d4dd5e4ffcb5abd01ffe52ac05e70dd7
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-27 22:33:40 +0530

    ADDED:To welcome message

---
+++ src/main/java/com/bridgelabz/Email.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Email {
+    public static void main(String[] args) {
+        System.out.println("Welcome Email valid and invalid problem");
+    }
+}
---
+++ src/test/java/com/bridgelabz/EmailTest.java
@@ -0,0 +1,5 @@
+package com.bridgelabz;
+
+public class EmailTest {
+    Email email = new Email();
+}
